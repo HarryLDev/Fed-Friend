dc9e77ea7134a714d73cdf55ea11a934b8ba4caa
Harry Liu <harry.s.liu@gmail.com>
2025-01-05 20:41:35 -0500

    .

--- src/app/firebase-init.ts
+++ src/app/firebase-init.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCX6Mlhkhs019A94kkk-TKGcGoKMCCQUEM",
-  authDomain: "fed-friend.firebaseapp.com",
-  projectId: "fed-friend",
-  storageBucket: "fed-friend.firebasestorage.app",
-  messagingSenderId: "387920466580",
-  appId: "1:387920466580:web:4c9fe4a541704c79355a81",
-  measurementId: "G-6P3D181X2W"
+  apiKey: 'AIzaSyCX6Mlhkhs019A94kkk-TKGcGoKMCCQUEM',
+  authDomain: 'fed-friend.firebaseapp.com',
+  projectId: 'fed-friend',
+  storageBucket: 'fed-friend.firebasestorage.app',
+  messagingSenderId: '387920466580',
+  appId: '1:387920466580:web:4c9fe4a541704c79355a81',
+  measurementId: 'G-6P3D181X2W'
 };
 
 // Initialize Firebase
