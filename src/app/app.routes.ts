6c2bf5cb2f85a227d0a36ccdc36f7895307ce994
Harry Liu <harry.s.liu@gmail.com>
2025-01-07 17:19:39 -0500

    worked on login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashComponent } from './dash/dash.component';
+import { SchedulePComponent } from './schedule-p/schedule-p.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashComponent },
+  { path: 'schedule/:petId', component: SchedulePComponent }
+];
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
     selector: 'app-login',
-    imports: [],
+    imports: [MatFormFieldModule, FormsModule, MatButtonModule, MatCardModule, MatIconModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
-}
+  login(){
+
+  }
+
+    hide = signal(true);
+    clickEvent(event: MouseEvent) {
+      this.hide.set(!this.hide());
+      event.stopPropagation();
+    }
+
+  }
+
+
+
