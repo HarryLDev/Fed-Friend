e0161fd7a0cc185cc02ee4cdfb0a302f5763f697
Harry Liu <harry.s.liu@gmail.com>
2025-01-09 13:06:36 -0500

    routed login to register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,10 +5,11 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-login',
-    imports: [MatFormFieldModule, FormsModule, MatButtonModule, MatCardModule, MatIconModule, MatInputModule],
+    imports: [MatFormFieldModule, FormsModule, MatButtonModule, MatCardModule, MatIconModule, MatInputModule, RouterLink],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css'
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-register',
-    imports: [],
+    imports: [MatFormFieldModule, FormsModule, MatButtonModule, MatCardModule, MatIconModule, MatInputModule, RouterLink],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
