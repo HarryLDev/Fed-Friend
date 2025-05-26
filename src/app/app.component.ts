f7e4c0bd3fc8b7703ff1614ac431c4b706c6fda3
Harry Liu <harry.s.liu@gmail.com>
2025-05-25 20:38:32 -0400

    created dash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DashComponent } from "./dash/dash.component";
+import { fadeAnimation } from './route-animations';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, DashComponent],
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    animations:[fadeAnimation]
 })
 export class AppComponent {
   title = 'Fed-Friend';
+
 }
